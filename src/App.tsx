e3bed70ae70225ed9d238b833cb2da489b36cd61
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2024-05-02 13:56:48 +0800

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,99 @@ import { useState } from 'react';
 import { Button } from '@mui/material';
 import token_logo from './assets/token_logo.gif';
 import mascot from './assets/correct_mascot.png';
+import { useEffect } from 'react';
+import mobileMascot from "./assets/mobileMascot.png"
 import './App.css';
 function App() {
     const [] = useState(0);
+    const orientation = useOrientation();
 
     return (
-        <div style={{ height: "88%" }}>
-            <div className="main"style={{ display: "flex", alignItems: "center",position:"relative" }}>
-                <div style={{ flex: "3", height: "100%" }}>
-                    <div style={{ background: "#182a34", borderRadius: "0 25px 25px 0", boxShadow: "0px 8px 20px rgba(149, 235, 174, 1), 0px -8px 20px rgba(149, 235, 174, 1)" }}>
-                        <div style={{ width: "90%", margin: "auto", display: "flex", flexDirection: "column" }}>
-                            <div style={{ display: "flex", flexDirection: 'row' }}>
-                                <p style={{ color: "#5cc884", fontSize: "5vw" }}>SOULANA </p>
-                                <img src={token_logo} style={{ height: "auto",maxWidth:"12%", margin: "auto", marginLeft: "0px" }} alt="" />
-                            </div>
-                            <div style={{ marginTop: "-6vw" }}>
-                                <p style={{ color: "#6bb98b", fontSize: "2vw" }}>Incentivised Memecoin Trading Experience</p>
-                            </div>
-                            <div style={{ marginTop: "-4vw" }}>
-                                <p style={{ color: "#b3eaaf", fontSize: "4vw" }}>To Be Announced...</p>
-                            </div>
-                            <div style={{ marginTop: "20px", paddingBottom: "60px", display: "flex" }}>
-                                <Button className="button_intro" style={{ background: "#213840", paddingTop: "1vw", paddingBottom: "1vw", width: "25%", fontSize: "1vw", borderRadius: "20px", textTransform: "none" }}>Contact Us</Button>
-                                <Button className="button_intro" style={{ background: "#213840", paddingTop: "1vw", paddingBottom: "1vw", width: "25%", marginLeft: "30%", fontSize: "1vw", borderRadius: "20px", textTransform: "none" }}>Docs</Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ flex: '2', overflow: 'hidden', marginTop: "21%" }}>
-                    <img src={mascot} style={{ height: "850px", marginLeft: "38%", visibility: "hidden" }} alt="" />
-                </div>
-            </div>
-
-            <div style={{position:"absolute",bottom:"0",right:"0"}}>
-            <img className='pic' src={mascot} style={{ height: "30%",maxWidth:"20vw"}} alt="" />
-
-            </div>
-        </div>
+        <>
+            {orientation.startsWith('portrait') ? <MobileComponent /> : <Desktop />}
+        </>
 
     );
 }
+function Desktop() {
+    return (<div style={{ height: "88%" }}>
+        <div className="main" style={{ display: "flex", alignItems: "center", position: "relative" }}>
+            <div style={{ flex: "3", height: "100%" }}>
+                <div style={{ background: "#182a34", borderRadius: "0 25px 25px 0", boxShadow: "0px 8px 20px rgba(149, 235, 174, 1), 0px -8px 20px rgba(149, 235, 174, 1)" }}>
+                    <div style={{ width: "90%", margin: "auto", display: "flex", flexDirection: "column" }}>
+                        <div style={{ display: "flex", flexDirection: 'row' }}>
+                            <p style={{ color: "#5cc884", fontSize: "5vw" }}>SOULANA </p>
+                            <img src={token_logo} style={{ height: "auto", maxWidth: "12%", margin: "auto", marginLeft: "0px" }} alt="" />
+                        </div>
+                        <div style={{ marginTop: "-6vw" }}>
+                            <p style={{ color: "#6bb98b", fontSize: "2vw" }}>Incentivised Memecoin Trading Experience</p>
+                        </div>
+                        <div style={{ marginTop: "-4vw" }}>
+                            <p style={{ color: "#b3eaaf", fontSize: "4vw" }}>To Be Announced...</p>
+                        </div>
+                        <div style={{ marginTop: "20px", paddingBottom: "60px", display: "flex" }}>
+                            <Button className="button_intro" style={{ background: "#213840", paddingTop: "1vw", paddingBottom: "1vw", width: "25%", fontSize: "1vw", borderRadius: "20px", textTransform: "none" }}>Contact Us</Button>
+                            <Button className="button_intro" style={{ background: "#213840", paddingTop: "1vw", paddingBottom: "1vw", width: "25%", marginLeft: "30%", fontSize: "1vw", borderRadius: "20px", textTransform: "none" }}>Docs</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ flex: '2', overflow: 'hidden', marginTop: "21%" }}>
+                <img src={mascot} style={{ height: "850px", marginLeft: "38%", visibility: "hidden" }} alt="" />
+            </div>
+        </div>
 
+        <div style={{ position: "absolute", bottom: "0", right: "0" }}>
+            <img className='pic' src={mascot} style={{ height: "30%", maxWidth: "20vw" }} alt="" />
+
+        </div>
+    </div>)
+}
+function MobileComponent() {
+    return (
+        <div>
+            <div style={{ display: "flex", flexDirection: "column",marginTop:"-40vw" }}>
+                <div style={{ background: "#182a34", borderRadius: "0 25px 25px 0", boxShadow: "0px 8px 20px rgba(149, 235, 174, 1), 0px -8px 20px rgba(149, 235, 174, 1)" }}>
+                    <div style={{ width: "90%", margin: "auto", display: "flex", flexDirection: "column" }}>
+                        <div style={{ display: "flex", flexDirection: 'row' }}>
+                            <p style={{ color: "#5cc884", fontSize: "3rem" }}>SOULANA </p>
+                            <img src={token_logo} style={{ height: "auto", maxWidth: "21%", margin: "auto", marginLeft: "0px" }} alt="" />
+                        </div>
+                        <div style={{ marginTop: "-4rem" }}>
+                            <p style={{ color: "#6bb98b", fontSize: "1rem" }}>Incentivised Memecoin Trading Experience</p>
+                        </div>
+                        <div style={{ marginTop: "-2rem" }}>
+                            <p style={{ color: "#b3eaaf", fontSize: "2rem" }}>To Be Announced...</p>
+                        </div>
+                        <div style={{ marginTop: "1rem", paddingBottom: "60px", display: "flex" }}>
+                            <Button className="button_intro" style={{ background: "#213840", paddingTop: "1rem", paddingBottom: "1rem", width: "25%", fontSize: "0.7rem", borderRadius: "20px", textTransform: "none" }}>Contact Us</Button>
+                            <Button className="button_intro" style={{ background: "#213840", paddingTop: "1rem", paddingBottom: "1rem", width: "25%", marginLeft: "30%", fontSize: "0.7rem", borderRadius: "20px", textTransform: "none" }}>Docs</Button>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ position:"absolute",bottom:"0" }}>
+                    <img  src={mobileMascot} style={{ height: "80%", width: "90%" }} alt="" />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+function useOrientation() {
+    const [orientation, setOrientation] = useState(window.screen.orientation.type);
+
+    useEffect(() => {
+        const handleOrientationChange = () => {
+            setOrientation(window.screen.orientation.type);
+        };
+
+        window.screen.orientation.addEventListener("change", handleOrientationChange);
+
+        return () => {
+            window.screen.orientation.removeEventListener("change", handleOrientationChange);
+        };
+    }, []);
+
+    return orientation;
+}
 export default App;
