fccd77c7040dc083f43262b7c5076495ceaf13c3
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2024-05-02 16:49:48 +0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -42,14 +42,14 @@ function Desktop() {
                 <div style={{ background: "#182a34", borderRadius: "0 25px 25px 0", boxShadow: "0px 8px 20px rgba(149, 235, 174, 1), 0px -8px 20px rgba(149, 235, 174, 1)" }}>
                     <div style={{ width: "90%", margin: "auto", display: "flex", flexDirection: "column" }}>
                         <div style={{ display: "flex", flexDirection: 'row' }}>
-                            <p style={{ color: "#5cc884", fontSize: "5vw" }}>SOULANA </p>
+                            <p style={{ color: "#5cc884", fontSize: "4vw" }}>SOULANA </p>
                             <img src={token_logo} style={{ height: "auto", maxWidth: "12%", margin: "auto", marginLeft: "0px" }} alt="" />
                         </div>
                         <div style={{ marginTop: "-6vw" }}>
-                            <p style={{ color: "#6bb98b", fontSize: "2vw" }}>Incentivised Memecoin Trading Experience</p>
+                            <p style={{ color: "#6bb98b", fontSize: "1.6vw" }}>Incentivised Memecoin Trading Experience</p>
                         </div>
                         <div style={{ marginTop: "-4vw" }}>
-                            <p style={{ color: "#b3eaaf", fontSize: "4vw" }}>To Be Announced...</p>
+                            <p style={{ color: "#b3eaaf", fontSize: "3vw" }}>To Be Announced...</p>
                         </div>
                         <div style={{ marginTop: "20px", paddingBottom: "60px", display: "flex" }}>
                             <Button className="button_intro" style={{ background: "#213840", paddingTop: "1vw", paddingBottom: "1vw", width: "25%", fontSize: "1vw", borderRadius: "20px", textTransform: "none" }}>Contact Us</Button>
@@ -76,14 +76,14 @@ function MobileComponent() {
                 <div style={{ background: "#182a34", borderRadius: "25px 25px 25px 25px", boxShadow: "0px 8px 20px rgba(149, 235, 174, 1), 0px -8px 20px rgba(149, 235, 174, 1)",width:"90%",margin:"auto" }}>
                     <div style={{ width: "90%", margin: "auto", display: "flex", flexDirection: "column" }}>
                         <div style={{ display: "flex", flexDirection: 'row' }}>
-                            <p style={{ color: "#5cc884", fontSize: "2.6rem" }}>SOULANA </p>
+                            <p style={{ color: "#5cc884", fontSize: "3rem" }}>SOULANA </p>
                             <img src={token_logo} style={{ height: "auto", maxWidth: "21%", margin: "auto", marginLeft: "0px" }} alt="" />
                         </div>
                         <div style={{ marginTop: "-4rem" }}>
-                            <p style={{ color: "#6bb98b", fontSize: "0.6rem" }}>Incentivised Memecoin Trading Experience</p>
+                            <p style={{ color: "#6bb98b", fontSize: "1rem" }}>Incentivised Memecoin Trading Experience</p>
                         </div>
                         <div style={{ marginTop: "-2rem" }}>
-                            <p style={{ color: "#b3eaaf", fontSize: "1.6rem" }}>To Be Announced...</p>
+                            <p style={{ color: "#b3eaaf", fontSize: "2rem" }}>To Be Announced...</p>
                         </div>
                         <div style={{ marginTop: "0.2rem", paddingBottom: "4rem", display: "flex" }}>
                             <Button className="button_intro" style={{ background: "#213840", paddingTop: "1rem", paddingBottom: "1rem", width: "35%", fontSize: "0.7rem", borderRadius: "20px", textTransform: "none" }}>Contact Us</Button>
